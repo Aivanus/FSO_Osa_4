71262fdafbe9571fdce8113f2417d1bd4038efc9
Aivanus <ivank1995@hotmail.com>
2018-08-08 16:58:56 +0300

    refaktorointi valmis (4.2)

--- index.js
+++ index.js
@@ -4,24 +4,9 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const morgan = require("morgan")
+const morgan = require('morgan')
 
-const Blog = mongoose.model('Blog', {
-	title: String,
-	author: String,
-	url: String,
-	likes: Number
-})
-
-module.exports = Blog
-
-morgan.token("body", (req, res) => {
-	return JSON.stringify(req.body)
-})
-
-app.use(cors())
-app.use(bodyParser.json())
-app.use(morgan(":method :url :body :status :res[content-length] - :response-time ms"))
+const blogsRouter = require('./controllers/blogs')
 
 if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config()
@@ -38,25 +23,18 @@ mongoose
 		console.log(err)
 	})
 
-app.get('/api/blogs', (request, response) => {
-	Blog
-		.find({})
-		.then(blogs => {
-			response.json(blogs)
-		})
+
+morgan.token("body", (req, res) => {
+	return JSON.stringify(req.body)
 })
 
-app.post('/api/blogs', (request, response) => {
-	const blog = new Blog(request.body)
+app.use(cors())
+app.use(bodyParser.json())
+app.use(morgan(':method :url :body :status :res[content-length] - :response-time ms'))
 
-	blog
-		.save()
-		.then(result => {
-			response.status(201).json(result)
-		})
-})
+app.use('/api/blogs', blogsRouter)
 
-const PORT =  process.env.PORT || 3003
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
